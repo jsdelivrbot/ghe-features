bb2764364d6d990c5ab6f298850258a5e1cd73af
Ryuichi Okumura <okuryu@okuryu.com>
2015-12-13 23:24:31 +0900

    2 indents

--- chrome/background.js
+++ chrome/background.js
@@ -5,20 +5,20 @@
  */
 /*global chrome*/
 (function () {
-    "use strict";
+  "use strict";
 
-    var url = "https://cdn.rawgit.com/okuryu/ghe-features/master/ghe-features.json";
-    var data;
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = "json";
-    xhr.onload = function () {
-        data = xhr.response;
-    };
-    xhr.open("GET", url, true);
-    xhr.send();
+  var url = "https://cdn.rawgit.com/okuryu/ghe-features/master/ghe-features.json";
+  var data;
+  var xhr = new XMLHttpRequest();
+  xhr.responseType = "json";
+  xhr.onload = function () {
+    data = xhr.response;
+  };
+  xhr.open("GET", url, true);
+  xhr.send();
 
-    chrome.runtime.onMessage.addListener(function (message, sender, sendMessage) {
-        sendMessage(data);
-    });
+  chrome.runtime.onMessage.addListener(function (message, sender, sendMessage) {
+    sendMessage(data);
+  });
 
 }());
--- chrome/ghe-features.js
+++ chrome/ghe-features.js
@@ -5,63 +5,63 @@
  */
 /*global chrome*/
 (function () {
-    "use strict";
+  "use strict";
 
-    function inject(meta, data) {
-        if (meta.length !== data.length) {
-            return false;
-        }
-
-        var baseUrl = "https://enterprise.github.com/releases";
-        var preMessage = "<li class=\"meta-item\"><span class=\"octicon octicon-mark-github\"></span>";
-        var postMessage = "</li>";
-        var message;
-
-        for (var i = 0, l = meta.length; i < l; i++) {
-            if (data[i] < 0) {
-                message = `<a href="${baseUrl}">Not available on GitHub Enterprise</a>`;
-            } else if (data[i]) {
-                message = `<a href="${baseUrl}/${data[i]}/notes">GitHub Enterprise v${data[i]}</a>`;
-            } else {
-                continue;
-            }
-            meta[i].innerHTML += `${preMessage} ${message}${postMessage}`;
-        }
+  function inject(meta, data) {
+    if (meta.length !== data.length) {
+      return false;
     }
 
-    function make(data) {
-        if (!data) {
-            return false;
-        }
+    var baseUrl = "https://enterprise.github.com/releases";
+    var preMessage = "<li class=\"meta-item\"><span class=\"octicon octicon-mark-github\"></span>";
+    var postMessage = "</li>";
+    var message;
 
-        var extracted = [];
-        var meta = document.querySelectorAll(".blog-post-meta");
-        meta = Array.prototype.slice.call(meta);
+    for (var i = 0, l = meta.length; i < l; i++) {
+      if (data[i] < 0) {
+        message = `<a href="${baseUrl}">Not available on GitHub Enterprise</a>`;
+      } else if (data[i]) {
+        message = `<a href="${baseUrl}/${data[i]}/notes">GitHub Enterprise v${data[i]}</a>`;
+      } else {
+        continue;
+      }
+      meta[i].innerHTML += `${preMessage} ${message}${postMessage}`;
+    }
+  }
 
-        switch (meta.length) {
-        case 0:
-            return false;
-        case 1:
-            if (typeof data[location.href] === "undefined") {
-                return false;
-            }
-            extracted.push(data[location.href]);
-            break;
-        default:
-            meta.forEach(function (m) {
-                var a = m.parentNode.querySelector(".blog-post-title a");
-                if (a && a.href) {
-                    extracted.push(data[a.href]);
-                }
-            });
-            break;
-        }
-
-        inject(meta, extracted);
+  function make(data) {
+    if (!data) {
+      return false;
     }
 
-    chrome.runtime.sendMessage("data", function (data) {
-        make(data);
-    });
+    var extracted = [];
+    var meta = document.querySelectorAll(".blog-post-meta");
+    meta = Array.prototype.slice.call(meta);
+
+    switch (meta.length) {
+    case 0:
+      return false;
+    case 1:
+      if (typeof data[location.href] === "undefined") {
+        return false;
+      }
+      extracted.push(data[location.href]);
+      break;
+    default:
+      meta.forEach(function (m) {
+        var a = m.parentNode.querySelector(".blog-post-title a");
+        if (a && a.href) {
+          extracted.push(data[a.href]);
+        }
+      });
+      break;
+    }
+
+    inject(meta, extracted);
+  }
+
+  chrome.runtime.sendMessage("data", function (data) {
+    make(data);
+  });
 
 }());
